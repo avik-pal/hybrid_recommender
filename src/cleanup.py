c157d3d4cb717174f21ce77020f1c728f828ccd2
Avik Pal <avikpal@iitk.ac.in>
2018-05-23 14:30:27 +0530

    Code to clean the netflix data

---
+++ src/cleanup.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import os
+
+movies = sorted(os.listdir("training_set"))
+chosen_movies = movies[:10000]
+
+print("Creating DataFrame")
+nuser = 2649429
+data = pd.DataFrame(index = range(nuser), columns = range(1))
+
+for i in range(len(chosen_movies)):
+    col_name = f"{i+1}"
+
+    data[col_name] = pd.Series(np.zeros(nuser), columns = range(1))
+
+    df = pd.read_csv('training_set/'+chosen_movies[i], header = None, skiprows = 1)
+
+    data[i][df[0]] = df[1]
+
+    print("Processing of ",col_name," is complete")
+
+data.to_csv("netflix_clean_data.csv")
